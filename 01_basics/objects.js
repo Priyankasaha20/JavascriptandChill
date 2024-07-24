5472838f49b26d55b472d8a462b4184dbf7e0b5b
Priyanka Saha <111238052+Priyankasaha20@users.noreply.github.com>
2024-07-24 14:48:37 +0000

    objects

---
+++ 01_basics/objects.js
@@ -0,0 +1,4 @@
+const Jsuser={
+    name:"priyanka",
+    
+}
--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,11 @@
 console.log("Priyanka");
+
+let isLoggedin=1;
+let booleanisLoggedin= Boolean(isLoggedin)
+
+console.log(booleanisLoggedin);
+//datatype conversion 
+//phle tumne koi sa bhi var declare kiya aur phir apne according uske type ko change kara "33"=number 
+//jab typeof(score) log karoge toh string aayga.
+
+//in case of boolean 1=true ""=false priyanka=true
